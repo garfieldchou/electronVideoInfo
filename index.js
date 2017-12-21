d8b1596dda2a112059a9e33a31570957d926888a
garfield <cmgarfield@gmail.com>
2017-12-21 16:29:17 +0800

    pull another property off the electron object- BrowserWindow, and create the browser window once the app is ready

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const electron = require('electron');
 
-const {app} = electron;
+const {app, BrowserWindow} = electron;
 
 app.on('ready', () => {
-  console.log('App is now ready');
+  new BrowserWindow({});
 });
