ea2c093b91bd6010580aa95cb2ed772aef5c1ba0
garfield <cmgarfield@gmail.com>
2017-12-24 23:20:03 +0800

    use mainWindow.webContents.send() to send data back to mainWindow

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const electron = require('electron');
 const ffmpeg = require('fluent-ffmpeg');
 
-const {app, BrowserWindow, ipcMain } = electron;
+const {app, BrowserWindow, ipcMain} = electron;
+
+let mainWindow;
 
 app.on('ready', () => {
-  const mainWindow = new BrowserWindow({});
+  mainWindow = new BrowserWindow({});
   mainWindow.loadURL(`file://${__dirname}/index.html`);
 });
 
 ipcMain.on('video:submit', (event, path) => {
   ffmpeg.ffprobe(path, (err, metadata) => {
-    console.log('Video length is: ', metadata.format.duration);
+    mainWindow.webContents.send(
+      'video:metadata',
+      metadata.format.duration
+    );
   });
 });
